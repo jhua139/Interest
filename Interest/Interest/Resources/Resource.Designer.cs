d2ba81f607bd4028fad581941f14fee8b4c54a85
dasarsh <dasarsh@gmail.com>
2017-08-11 00:29:08 +1200

    added new percentage badge icon

--- Interest/Interest/Resources/Resource.Designer.cs
+++ Interest/Interest/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace Interest
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int discount = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
