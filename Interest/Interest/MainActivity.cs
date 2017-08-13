80a4055218904af9bb72d0011a72e3a078a049ab
dasarsh <dasarsh@gmail.com>
2017-08-13 18:46:52 +1200

    Added four textviews in scrollviews for four banks

--- Interest/Interest/MainActivity.cs
+++ Interest/Interest/MainActivity.cs
@@ -7,15 +7,20 @@ namespace Interest
     [Activity(Label = "Interest", MainLauncher = true, Icon = "@drawable/discount")]
     public class MainActivity : Activity
     {
+        TextView tvAsb, tvAnz, tvBnz, tvWestpac;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            tvAsb = (TextView) FindViewById(Resource.Id.textViewAsb);
+            tvAnz = (TextView)FindViewById(Resource.Id.textViewAnz);
+            tvBnz = (TextView)FindViewById(Resource.Id.textViewBnz);
+            tvWestpac = (TextView)FindViewById(Resource.Id.textViewWestpac);
         }
+        
     }
 }
 
--- Interest/Interest/Resources/Resource.Designer.cs
+++ Interest/Interest/Resources/Resource.Designer.cs
@@ -60,6 +60,37 @@ namespace Interest
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int scrollViewAnz = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int scrollViewAsb = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int textViewAnz = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int textViewAsb = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int textViewBnz = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int textViewWestpac = 2131034117;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
