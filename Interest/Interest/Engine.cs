565514ed35467848a22ff2dcc3bd6957fcfac434
dasarsh <dasarsh@gmail.com>
2017-08-13 19:40:31 +1200

    Added engine class and calling it in click listener in main activity for when textview is tapped

---
+++ Interest/Interest/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Interest
+{
+    class Engine
+    {
+        public void GetAnzData()
+        {
+            // TODO: API call here
+        }
+    }
+}
--- Interest/Interest/MainActivity.cs
+++ Interest/Interest/MainActivity.cs
@@ -8,6 +8,9 @@ namespace Interest
     public class MainActivity : Activity
     {
         TextView tvAsb, tvAnz, tvBnz, tvWestpac;
+        ScrollView svAsb, svAnz, svBnz, svWestpac;
+
+        Engine engine;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -15,12 +18,25 @@ namespace Interest
 
             SetContentView(Resource.Layout.Main);
 
-            tvAsb = (TextView) FindViewById(Resource.Id.textViewAsb);
+            tvAsb = (TextView)FindViewById(Resource.Id.textViewAsb);
             tvAnz = (TextView)FindViewById(Resource.Id.textViewAnz);
             tvBnz = (TextView)FindViewById(Resource.Id.textViewBnz);
             tvWestpac = (TextView)FindViewById(Resource.Id.textViewWestpac);
+
+            svAsb = (ScrollView)FindViewById(Resource.Id.scrollViewAsb);
+            svAnz = (ScrollView)FindViewById(Resource.Id.scrollViewAnz);
+            svBnz = (ScrollView)FindViewById(Resource.Id.scrollViewBnz);
+            svWestpac = (ScrollView)FindViewById(Resource.Id.scrollViewWestpac);
+
+            tvAsb.Click += Asb_Click;
+
+            engine = new Engine();
         }
-        
+
+        private void Asb_Click(object sender, System.EventArgs e)
+        {
+            engine.GetAnzData();
+        }
     }
 }
 
