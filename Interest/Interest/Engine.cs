5e8180672d11af3da1963fb461989ddef6939e18
John Huang <jhua139@gmail.com>
2017-08-16 20:42:49 +1200

    s

--- Interest/Interest/Engine.cs
+++ Interest/Interest/Engine.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using HtmlAgilityPack;
 
 namespace Interest
 {
     class Engine
     {
-        public void GetAsbData()
-        {
-            // TODO: API call here
-        }
-
         public void GetAnzData()
         {
+            {
+                var html = @"http://www.anz.co.nz/";
+
+                HtmlWeb web = new HtmlWeb();
+
+                var htmlDoc = web.Load(html);
+
+                var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+
+                string text = node.OuterHtml.ToString();
+               
+                return text;
+
+            }
             // TODO: API call here
         }
 
